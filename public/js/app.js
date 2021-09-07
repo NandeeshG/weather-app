99b35f4cde188902ad5e53acbc3710c478470e20
NandeeshG <nandeeshgupta@gmail.com>
2021-09-08 02:03:52 +0530

    Weather front end working

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,54 @@
-console.log('Client Side JS Loaded')
+const baseurl = 'http://localhost:3000'
+let weatherEndpoint = new URL('weather',baseurl)
+const loadingP = document.getElementById('loading-p')
+
+function weatherFetch(city,cb){
+    let param = new URLSearchParams({city})
+    let url = weatherEndpoint + '?' + param
+    //let url = 'http://puzzle.mead.io/puzzle'
+    loadingP.innerText = 'Loading...'
+    fetch(url)
+    .then((res)=>{
+        loadingP.innerText = ''
+        if(res.error){
+            return cb(res.error)
+        }
+        res.json()
+        .then((rj)=>{
+            if(rj.error) return cb(rj.error)
+            cb(null,rj)
+        })
+        .catch((err)=>cb('There was an Error!',err))
+    })
+    .catch((err)=>{
+        loadingP.innerText = ''
+        return cb('There was an Error!',err)
+    })
+}
+
+const weatherForm = document.querySelector('form')
+const cityQuery = document.getElementById('city')
+const resultsContainer = document.getElementById('search-results')
+
+weatherForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    weatherFetch(cityQuery.value, (err,res) => {
+        if(err){
+            loadingP.innerText = 'Can\'t Fetch, '+(err.code||err.cod||err.error||err)
+        }else{
+            const newD = document.createElement('div')
+
+            const newH = document.createElement('h3')
+            newH.innerText = res.summary
+            newD.appendChild(newH)
+
+            const newS = document.createElement('h5')
+            newS.innerText = `(Lat,Long):(${res.latitude},${res.longitude})`
+            newD.appendChild(newS)
+
+            newD.style.border = '1px dotted grey';
+
+            resultsContainer.prepend(newD)
+        }
+    })
+})
--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const weather = require('./weather/weather').module
+const weather_utils = require('./weather/utils').module
+const { callbackify } = require('util')
 
 const app = express()
 
@@ -20,23 +23,117 @@ app.get('', (req, res) => {
     res.render('index', {
         title: 'WeatherApp',
         author: 'Nandeesh Gupta',
+        //indexText: 'Enter the name of the city below'
     })
 })
 app.get('/help', (req, res) => {
     res.render('help', {
-        title: 'Help page',
+        title: 'Help',
         author: 'Nandeesh Gupta',
-        helpText: 'Help has arrived!',
+        helpText: 'To use the app, go to home page and enter the city name\'s whose current weather you are looking for. Forecast weather coming soon!',
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'About this app',
+        title: 'About the app',
         author: 'Nandeesh Gupta',
+        aboutText: 'This app uses OpenWeatherAPI, NodeJS, ExpressJS, Handlebars and HTML,CSS to provide you the current weather details. Made by me in Sep 2021',
         linkedin: 'nandeeshg',
     })
 })
+
+app.get('/weather', (req,res) => {
+    if(!req.query.city){
+        return res.send({
+            error:"City not provided!"
+        })
+    }
+    weather.geocoding(req.query.city,(err,data)=>{
+        if(err){
+            return res.send({
+                error:err
+            })
+        }
+        let ret_data ={
+            city: data.name,
+            longitude: data.coord.lon,
+            latitude: data.coord.lat,
+            weather_summary: data.weather[0].description,
+            weather_main: data.weather[0].main,
+            temperature: weather_utils.specialTransform('temp',data.main.temp),
+            feels_like: weather_utils.specialTransform('temp',data.main.feels_like),
+            humidity: weather_utils.specialTransform('humidity',data.main.humidity),
+            visibility: weather_utils.specialTransform('humidity',data.visibility),
+            wind_speed: weather_utils.specialTransform('speed',data.wind.speed),
+            clouds: weather_utils.specialTransform('clouds',data.clouds.all),
+            dt: weather_utils.specialTransform('dt',data.dt),
+            sunrise: weather_utils.specialTransform('dt',data.sys.sunrise),
+            sunset: weather_utils.specialTransform('dt' ,data.sys.sunset),
+            country_code: data.sys.country,
+        }
+        ret_data.summary = `${ret_data.city} is experiencing ${ret_data.weather_summary||ret_data.weather_main||'the following'}. Temperature is ${ret_data.temperature}, which feels like ${ret_data.feels_like} due to ${ret_data.humidity} Humidity.`
+        res.send(ret_data)
+    })
+})
+
+app.get('/country', (req,res) => {
+    // https://restcountries.eu/rest/v2/alpha/BT
+    res.send({
+        error: "Endpoint coming soon!"
+    })
+})
+
+app.get('/forecast', (req,res) => {
+    if(!req.query.city){
+        return res.send({
+            error:"City not provided!"
+        })
+    }
+    req.query.type = req.query.type || 'C'
+
+    if (!(req.query.type in weather.maptypes)) {
+        return res.send({
+            error:"Wrong forecast type!"
+        })
+    }
+    weather.geocoding(req.query.city, (err, resw) => {
+        if (err){
+            return res.send({error:err})
+        }
+        else {
+            weather.forecast(
+                resw.coord.lat,
+                resw.coord.lon,
+                (err, resw) => {
+                    if (err){
+                        return res.send({
+                            error:err
+                        })
+                    } 
+                    const recursiveTransform = (item,obj) => {
+                        if(typeof obj[item] === 'object'){
+                            for(let i in obj[item])
+                                recursiveTransform(i,obj[item])
+                        }
+                        else{
+                            obj[item] = weather_utils.specialTransform(item,obj[item])
+                        }
+                    }
+                    for(let item in resw){
+                        recursiveTransform(item, resw)
+                    }
+                    return res.send(resw)
+                },
+                weather.maptypes[req.query.type]
+            )
+        }
+    })
+})
+
+
+
+//----- 404s 
 
 app.get('/about/*', (req, res) => {
     res.render('404', {
